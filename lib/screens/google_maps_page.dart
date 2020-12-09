ba82b413b6a04cc712011204e45dde8abe5f794c
Mehmed ISMAIL <atcyazilim@gmail.com>
2020-12-09 18:00:25 +0300

    Marker eklendi

--- lib/screens/google_maps_page.dart
+++ lib/screens/google_maps_page.dart
@@ -1,24 +1,88 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:kupe/constants.dart';
+import 'package:location/location.dart';
+import 'dart:collection';
 
 class GoogleMapsPage extends StatefulWidget {
   static const String id = 'google_maps_page';
+  final LocationData location;
+  GoogleMapsPage({this.location});
 
   @override
   _GoogleMapsPageState createState() => _GoogleMapsPageState();
 }
 
 class _GoogleMapsPageState extends State<GoogleMapsPage> {
-  //Completer<GoogleMapController> _mapController = Completer();
-  GoogleMapController mapController;
   MapType _mapType = MapType.normal;
+  //Location
+  LocationData _locationData;
+
+  //Maps
+  Set<Marker> _markers = HashSet<Marker>();
+  GoogleMapController _googleMapController;
+  BitmapDescriptor _markerIcon;
+
+  //id
+  int _markerIdCounter = 1;
+
+  //Type controller
+  bool _isMarker = false;
+
+  @override
+  void initState() {
+    super.initState();
+    //Marker ikonunu değiştirmek istersem
+    _setMarkerIcon();
+    _locationData = widget.location;
+    print('Google Maps Page de........');
+    print('Lat Long buraya gelmeli... $_locationData');
+  }
+
+  //Marker ikonunu değiştirmek için
+  void _setMarkerIcon() async {
+    _markerIcon = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(), 'images/kopek.png');
+  }
+
+  //Set Markers to the map
+  void _setMarkers(LatLng point) {
+    final String markerIdVal = 'marker_id_$_markerIdCounter';
+    _markerIdCounter++;
+    setState(() {
+      print(
+          'Marker | Latitude: ${point.latitude} Longitude: ${point.longitude}');
+      _markers.add(
+        Marker(
+          markerId: MarkerId(markerIdVal),
+          position: point,
+        ),
+      );
+    });
+  }
 
   //default coordinates set to Istanbul
-  final LatLng _center = const LatLng(41.015137, 28.979530);
+  //final LatLng _center = const LatLng(41.015137, 28.979530);
 
+  //Mapi bu marker ayarlarıyla başlat
   void _onMapCreated(GoogleMapController controller) {
-    //_mapController.complete(controller);
-    mapController = controller;
+    _googleMapController = controller;
+
+    setState(() {
+      _markers.add(
+        Marker(
+          markerId: MarkerId('0'),
+          position: LatLng(41.015137, 28.979530),
+          infoWindow: InfoWindow(
+            title: 'Hayvanın adı gelecek',
+            snippet: 'Deneme altyazısıdır',
+            //buraya bizim hayvanların bilgileri gelecek, popup şeklinde
+            onTap: () {},
+          ),
+          //icon: _markerIcon,
+        ),
+      );
+    });
   }
 
   @override
@@ -26,21 +90,29 @@ class _GoogleMapsPageState extends State<GoogleMapsPage> {
     return Stack(
       children: [
         GoogleMap(
-          //choose type of the map
-          mapType: _mapType,
-          onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
-            target: _center,
+            target: LatLng(_locationData.latitude, _locationData.longitude),
             zoom: 11.0,
           ),
-          zoomControlsEnabled: true,
+          mapType: _mapType,
+          markers: _markers,
+          myLocationEnabled: true,
+          onTap: (point) {
+            if (_isMarker) {
+              setState(() {
+                _markers.clear();
+                _setMarkers(point);
+              });
+            } else {}
+          },
+          onMapCreated: _onMapCreated,
+          //zoomControlsEnabled: true,
         ),
         Padding(
-          padding: EdgeInsets.all(16.0),
+          padding: EdgeInsets.symmetric(horizontal: 13.0, vertical: 16.0),
           child: Align(
-            alignment: Alignment.bottomCenter,
+            alignment: Alignment.bottomLeft,
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 CircleAvatar(
                   radius: 28.0,
@@ -55,7 +127,7 @@ class _GoogleMapsPageState extends State<GoogleMapsPage> {
                         });
                       }),
                 ),
-                SizedBox(width: 16.0),
+                SizedBox(width: 13.0),
                 CircleAvatar(
                   radius: 28.0,
                   backgroundColor: Colors.white.withOpacity(0.75),
@@ -69,6 +141,16 @@ class _GoogleMapsPageState extends State<GoogleMapsPage> {
                         });
                       }),
                 ),
+                SizedBox(width: 13.0),
+                //Marker için ornek button
+                RaisedButton(
+                    color: kMainKupeColor,
+                    onPressed: () {
+                      _isMarker = true;
+                    },
+                    child: Text('Marker',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.white))),
               ],
             ),
           ),
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -2,9 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:kupe/constants.dart';
 import 'package:kupe/widgets/nav_menu.dart';
 import 'package:kupe/screens/google_maps_page.dart';
+import 'package:location/location.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   static const String id = 'home_page';
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Location location = Location();
+  bool _serviceEnabled;
+  PermissionStatus _permissionGranted;
+  LocationData _locationData;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkLocationPermission();
+    print('home page de');
+    print(_locationData);
+  }
+
+  //Check Location Permissions and get my location
+  void _checkLocationPermission() async {
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+    _locationData = await location.getLocation();
+
+    print('Fonksyonunu içinde..');
+    print(_locationData);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +59,24 @@ class HomePage extends StatelessWidget {
         title: Image.asset('images/yazi_logo.png', fit: BoxFit.contain),
       ),
       //google maps sayfasına git
-      body: GoogleMapsPage(),
+      body:
+
+          /*RaisedButton(
+        onPressed: () => _locationData != null
+            ? Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => GoogleMapsPage(
+                          location: _locationData,
+                        )))
+            : null,
+      ),*/
+
+          //GoogleMapsPage(location: _locationData),
+
+          _locationData != null
+              ? GoogleMapsPage(location: _locationData)
+              : null,
     );
   }
 }
